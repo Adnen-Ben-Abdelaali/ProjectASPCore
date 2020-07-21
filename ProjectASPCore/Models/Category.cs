ccda65c71ec4a89d0732c3f6fd813b72c7baddec
Adnen Ben Abdelaali <adanino216@outlook.com>
2020-07-21 10:28:25 +0200

    first commit

---
+++ ProjectASPCore/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectASPCore.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string description { get; set; }
+        public List<Book>
+    }
+}
