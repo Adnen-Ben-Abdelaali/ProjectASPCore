1d57bb37588c9142ad1393cb4c1b9f622ab042a4
Adnen Ben Abdelaali <adanino216@outlook.com>
2020-07-21 13:52:23 +0200

    View List.cshtml associé à BookController

--- ProjectASPCore/Controllers/BookController.cs
+++ ProjectASPCore/Controllers/BookController.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjectASPCore.Models;
+using ProjectASPCore.Repository;
 
 namespace ProjectASPCore.Controllers
 {
     public class BookController : Controller
     {
-     
+        private readonly IBookRepository _bookRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public BookController(IBookRepository bookRepository, ICategoryRepository categoryRepository) {
+
+            _bookRepository = bookRepository;
+            _categoryRepository = categoryRepository;
+
+        }
+
+        public IActionResult List() {
+            var books = _bookRepository.GetAllBooks();
+
+            return View(books);
+        }
     }
 }
